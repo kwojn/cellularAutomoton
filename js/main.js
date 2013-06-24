c7d18012a7ed4f5adb06d52ea37081d71957d9a0
krzysztof <krzysztof@localhost.localdomain>
2013-06-24 12:44:31 +0200

    Code cleanup

--- js/main.js
+++ js/main.js
@@ -9,76 +9,6 @@
 			immunity:0.0,
 		},
 		
-		
-		/**
-		 *  [0] - immunity flag
-		 *  0-immune,1-healthy,2-sick,3-dead
-		 *  [1] - stamina
-		 *  
-		 * @param {type} refferenceElement
-		 * @param {type} surroundingElemenet
-		 */
-		transitionDispatch:function(surrounding){
-			var pointer=0;
-			var cell= new Array(); 
-			for (var y=0;y<3;y++){
-				for (var x=0;x<3;x++){
-					
-					if (pointer==4*4){
-						pointer+=4;
-						continue;
-					}
-					
-					
-					// mark me if I'm dead
-					if (surrounding.data[(4*4)+1]<=0){
-							surrounding.data[(4*4)]=3;
-							surrounding.data[(4*4)+3]=0;
-							return new Array(surrounding.data[(4*4)],
-										surrounding.data[(4*4+1)],
-										surrounding.data[(4*4+2)],
-										surrounding.data[(4*4+3)]);
-					}
-					
-					// here we add transition rules
-					
-					//get sick if neighbour is sick
-					if (surrounding.data[pointer+0]==2){
-						if (surrounding.data[4*4]!=0){
-							surrounding.data[4*4]=2;
-						}
-					}
-					
-					//get energy if neighbour is dead
-					if (surrounding.data[pointer+0]!=2){
-							surrounding.data[(4*4)]=1;
-							surrounding.data[(4*4)+1]+=10;
-						
-					}
-					
-					//decrease energy if I'm sick
-					if (surrounding.data[(4*4)]==2){
-							surrounding.data[(4*4)+1]-=10;
-					}
-					
-					
-					
-					// mark me healed if my energy is above 250
-					if (surrounding.data[(4*4)+1]>=250){
-							surrounding.data[(4*4)]=1;
-					}
-					
-					pointer+=4;
-				}
-			}	
-			return new Array(surrounding.data[(4*4)],
-										surrounding.data[(4*4+1)],
-										surrounding.data[(4*4+2)],
-										surrounding.data[(4*4+3)]);
-			
-			
-		},
-		
 		initCanvas:function(containingId){
 			if (typeof this.canvas=='undefined'){
 				this.canvas = document.createElement('canvas');
@@ -91,8 +21,7 @@
 			
 			
 		},
-		
-		
+	
 		initPopulation:function(){
 			var imageData = this.context.createImageData(this.config.x,this.config.y);
 			
@@ -101,7 +30,7 @@
 				for(var x=0;x<this.config.x;x++){
 					 
 					imageData.data[i]=this.config.immunity<Math.random()?(1+Math.round(2*Math.random())):0;
-					imageData.data[i+1]=250;//Math.round(255*Math.random());
+					imageData.data[i+1]=Math.round(255*Math.random());
 					imageData.data[i+2]=imageData.data[i]==0?255:0;
 					imageData.data[i+3]=255;
 					i+=4;
@@ -109,7 +38,7 @@
 					
 				}
 			}
-			//console.log(imageData,this.config.x,this.config.y);
+			
 			this.context.putImageData(imageData,0,0);
 			
 			
@@ -123,6 +52,7 @@
 				for(var x=0;x<=imageData.width;x++){
 					var heal=0;
 					var imune=0;
+					// here we have transition rules hardcoded - please be kind and export them to separate method :)
 					for  (var c=pointer-(virusHandler.config.x*4);c<=pointer+(virusHandler.config.x*4);c+=virusHandler.config.x*4){
 					for (var k=-4;k<=4;k+=4){
 						if (k==0){
@@ -176,16 +106,7 @@
 		
 		},
 		
-		
-			
-		
-		
 	};
-	
-	
-	
-	
-	
 	
 })(this);
 
